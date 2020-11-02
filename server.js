45e004e3e3726e54040665577a6221dd44363cbd
@Durjoy007 <mrdatta2410@gmail.com>
2020-11-02 14:57:58 +0600

    currently working

---
+++ server.js
@@ -0,0 +1,27 @@
+// Imports
+const express = require("express")
+const app = express()
+const port = process.env.port || 3000
+
+
+// Static Files
+app.use(express.static("public"))
+app.use("/css", express.static(__dirname + "public/css"))
+app.use("/js", express.static(__dirname + "public/js"))
+
+
+// Set Views
+app.set("views", "./views")
+app.set("view engine", "ejs")
+
+
+// Sending Files on Request
+app.get("/", function(req, res) {
+    res.render("index")
+})
+
+
+// Listens
+app.listen(port, function(req, res) {
+    console.log(`Server is connected on ${port}`)
+})
